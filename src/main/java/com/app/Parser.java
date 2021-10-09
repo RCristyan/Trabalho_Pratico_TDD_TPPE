262f9f1b1b6392a367719d47eb63906f3599e4f6
RCristyan <rcristyan9@gmail.com>
2021-10-09 17:22:50 -0300

    Implementados construtores padrão e alternativo.

--- src/main/java/com/app/Parser.java
+++ src/main/java/com/app/Parser.java
@@ -4,12 +4,17 @@ public class Parser {
 	// Responsável por realizar o parse das entradas
 	private char delimiter = '-';
 	
+	public Parser() {}
+	
+	public Parser(char delimiter) {
+		this.delimiter = delimiter;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
 
 	public char getDelimiter() {
-		// TODO Auto-generated method stub
 		return this.delimiter;
 	}
 
