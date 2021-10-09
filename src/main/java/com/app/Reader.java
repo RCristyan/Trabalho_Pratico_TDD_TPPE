700658d449023daa878d209e39b2c8c3fda16099
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-10-08 21:57:42 -0300

    Criando caso de teste para classe Reader

--- src/main/java/com/app/Reader.java
+++ src/main/java/com/app/Reader.java
@@ -2,4 +2,14 @@ package com.app;
 
 public class Reader {
 	// Responsável pela leitura dos arquivos de entrada do programa
+
+	Reader(String fileName) {
+
+	}
+	
+	String[] read() {
+		// TODO - implement Reader.read
+		String [] res = { "", ""};
+		return res;
+	}
 }
---
+++ src/test/java/com/app/ReaderTest.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReaderTests {
+    @Test
+    void testRead() {
+        Reader reader = new Reader("src/test/resources/test.txt");
+        String[] expected = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] actual = reader.read();
+        assertArrayEquals(expected, actual);
+    }
+}
