70406c066c61f15f1bfafb7a4c02202f0c8648c1
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-10-18 09:11:31 -0300

    Criando teste para escrita no arquivo
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>

--- src/test/java/com/app/WriterTest.java
+++ src/test/java/com/app/WriterTest.java
@@ -130,4 +130,28 @@ public class WriterTest {
         // Clean
         path.toFile().delete();
     }
+
+    @Test
+    public void TestWriterIsWritingContent() {
+        Writer writer = new Writer();
+        try {
+            writer.setOutputPath("src/test/resources");
+            writer.write("file.txt", "Hello World!");
+        } catch(Exception ex){
+            fail(ex.getMessage());
+        }
+        Path path = Paths.get("src/test/resources/file.txt");
+        BufferedReader reader;
+        String content = "";
+        try {
+            reader = new BufferedReader(new FileReader(path.toFile()));
+            content = reader.readLine();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        assertEquals("Hello World!", content);
+
+        // Clean
+        path.toFile().delete();
+    }
 }
